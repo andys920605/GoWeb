6f277ad2cf59e7ffee52f3b0bc1654480d4a3298
Andy <andy_chang@protech.com.tw>
2022-08-04 15:42:49 +0800

    fix issue

--- service/login_service.go
+++ service/login_service.go
@@ -2,15 +2,10 @@ package service
 
 import (
 	rep "GoWeb/repository/postgredb"
-	"time"
 )
 
 type ILoginSrv interface {
 }
-
-var (
-	cancelTimeout time.Duration = 3 // default 3 second
-)
 
 type LoginSrv struct {
 	MemberRepo rep.IMemberRepo

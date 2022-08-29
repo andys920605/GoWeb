078f73df0978c06dae7fd4f2ab98b1ba7b2e616f
Andy <andy_chang@protech.com.tw>
2022-08-29 13:42:37 +0800

    add member repository testing

--- go.mod
+++ go.mod
@@ -5,8 +5,9 @@ go 1.18
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/gin-gonic/gin v1.8.1
-	github.com/go-chi/render v1.0.1
+	github.com/go-chi/render v1.0.2
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-test/deep v1.0.8
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/mock v1.6.0
 	github.com/jinzhu/gorm v1.9.16
@@ -15,11 +16,12 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.12.0
-	github.com/stretchr/testify v1.7.1
+	github.com/stretchr/testify v1.8.0
 	github.com/xorcare/pointer v1.2.1
 )
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -54,5 +56,5 @@ require (
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- repository/postgredb/memberRepo_test.go
+++ repository/postgredb/memberRepo_test.go
@@ -5,15 +5,18 @@ import (
 	"log"
 	"regexp"
 	"testing"
+	"time"
 
 	models_rep "GoWeb/models/repository"
 	"GoWeb/repository/postgredb"
 	"GoWeb/utils"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-test/deep"
 	"github.com/golang/mock/gomock"
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/xorcare/pointer"
 )
 
@@ -21,7 +24,6 @@ func TestMemberRepo_Insert(t *testing.T) {
 	t.Parallel()
 	mockCtl := gomock.NewController(t)
 	defer mockCtl.Finish()
-	//mockLogger := mock.NewMockIApiLogger(mockCtl)
 	db, mock := newMemberMock()
 	repository := postgredb.NewMemberRepo(db)
 	defer func() {
@@ -34,6 +36,85 @@ func TestMemberRepo_Insert(t *testing.T) {
 		WillReturnResult(sqlmock.NewResult(1, 1))
 	err := repository.Insert(context.Background(), want)
 	assert.NoError(t, err)
+}
+
+func TestMemberRepo_Find(t *testing.T) {
+	t.Parallel()
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	db, mock := newMemberMock()
+	repository := postgredb.NewMemberRepo(db)
+	defer func() {
+		repository.Close()
+	}()
+	want := createRandomMemberAccount()
+	query := `SELECT account,password,permission,name,email,phone,address,create_at,update_at FROM member` +
+		` WHERE is_alive = true AND account =$1 OR phone = $2 ORDER BY id`
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(want.Account, want.Phone).
+		WillReturnRows(sqlmock.NewRows([]string{"account,", "password", "permission", "name", "email", "phone", "address", "create_at", "update_at"}).
+			AddRow(want.Account, want.Password, want.Permission, want.Name, want.Email, want.Phone, want.Address, want.CreateAt, want.UpdateAt))
+	got, err := repository.Find(context.Background(), &want.Account, want.Phone)
+	require.NoError(t, err)
+	require.Nil(t, deep.Equal(want, got))
+}
+
+func TestMemberRepo_FindAll(t *testing.T) {
+	t.Parallel()
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	db, mock := newMemberMock()
+	repository := postgredb.NewMemberRepo(db)
+	defer func() {
+		repository.Close()
+	}()
+	want := []models_rep.Member{}
+	want = append(want, *createRandomMemberAccount())
+	want = append(want, *createRandomMemberAccount())
+	query := `SELECT account,password,permission,name,email,phone,address,create_at,update_at
+	FROM member
+	WHERE is_alive = true`
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WillReturnRows(sqlmock.NewRows([]string{"account,", "password", "permission", "name", "email", "phone", "address", "create_at", "update_at"}).
+			AddRow(want[0].Account, want[0].Password, want[0].Permission, want[0].Name, want[0].Email, want[0].Phone, want[0].Address, want[0].CreateAt, want[0].UpdateAt).
+			AddRow(want[1].Account, want[1].Password, want[1].Permission, want[1].Name, want[1].Email, want[1].Phone, want[1].Address, want[1].CreateAt, want[1].UpdateAt))
+	got, err := repository.FindAll(context.Background())
+	require.NoError(t, err)
+	require.Nil(t, deep.Equal(&want, got))
+}
+
+func TestMemberRepo_Updates(t *testing.T) {
+	t.Parallel()
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	db, mock := newMemberMock()
+	repository := postgredb.NewMemberRepo(db)
+	defer func() {
+		repository.Close()
+	}()
+	want := createRandomUpdateMemberAccount()
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE member SET name =$2, email =$3,phone=$4, address=$5, update_at=$6 WHERE account = $1`)).
+		WithArgs(want.Account, want.Name, want.Email, want.Phone, want.Address, time.Now().Format(time.RFC3339)).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	err := repository.Updates(context.Background(), want)
+	require.NoError(t, err)
+}
+
+func TestMemberRepo_Disable(t *testing.T) {
+	t.Parallel()
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	db, mock := newMemberMock()
+	repository := postgredb.NewMemberRepo(db)
+	defer func() {
+		repository.Close()
+	}()
+	want := createRandomUpdateMemberAccount()
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE member SET is_alive = false WHERE account = $1`)).
+		WithArgs(want.Account).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	err := repository.Disable(context.Background(), want)
+	require.NoError(t, err)
 }
 
 // region private methods
@@ -54,11 +135,22 @@ func createRandomMemberAccount() *models_rep.Member {
 	return &models_rep.Member{
 		Account:    "test_" + utils.Rand(10, utils.RAND_KIND_ALL),
 		Password:   utils.Rand(10, utils.RAND_KIND_ALL),
-		Permission: utils.GenerateRangeNum(2, 4),
+		Permission: 2,
 		Name:       utils.Rand(10, utils.RAND_KIND_ALL),
 		Email:      pointer.String(utils.RandomEmail()),
 		Phone:      pointer.String("09" + utils.RandomNum(8)),
 		Address:    pointer.String(utils.Rand(15, utils.RAND_KIND_ALL)),
+		CreateAt:   pointer.Time(time.Now()),
+		UpdateAt:   pointer.Time(time.Now()),
+	}
+}
+func createRandomUpdateMemberAccount() *models_rep.UpdateMember {
+	return &models_rep.UpdateMember{
+		Account: "test_" + utils.Rand(10, utils.RAND_KIND_ALL),
+		Name:    pointer.String(utils.Rand(10, utils.RAND_KIND_ALL)),
+		Email:   pointer.String(utils.RandomEmail()),
+		Phone:   pointer.String("09" + utils.RandomNum(8)),
+		Address: pointer.String(utils.Rand(15, utils.RAND_KIND_ALL)),
 	}
 }
 
